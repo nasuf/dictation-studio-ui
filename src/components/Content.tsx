d62ccafe1c3dfcf1cf338aab4f37110fe8137ab9
nasuf <shanian713@gmail.com>
2025-09-04 13:29:09 +0800

    enhance translations

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -594,13 +594,13 @@ const AppContent: React.FC<AppContentProps> = ({
                 <button
                   onClick={() => setIsSettingsModalVisible(true)}
                   className="flex items-center justify-center p-2 md:px-4 md:py-2 bg-gray-500 text-white shadow-md rounded-md hover:bg-gray-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
-                  title="Player Settings"
+                  title={t("playerSettings")}
                 >
                   <svg className="w-4 h-4 md:mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
-                  <span className="hidden md:inline">Settings</span>
+                  <span className="hidden md:inline">{t("settings")}</span>
                 </button>
               </div>
             )}
